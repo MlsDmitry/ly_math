2e3a0e8dda1ff6f6e4eca65a6e69b8d581aaa8fc
Dmitry Molokovich <molokovich.dima@gmail.com>
2020-11-24 20:41:52 +0300

    clean up

--- src/main.py
+++ src/main.py
@@ -1,9 +1,3 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QGridLayout, QPushButton, QLineEdit, QLabel, QVBoxLayout, QCalendarWidget, QHBoxLayout
-from PyQt5.QtGui import QIntValidator
-import sys
-
-from src.custom_widgets import CheckableComboBox, StackedWidget
-from src.logger.logger import Log
 from src.module.controller import Controller
 
 class App:
